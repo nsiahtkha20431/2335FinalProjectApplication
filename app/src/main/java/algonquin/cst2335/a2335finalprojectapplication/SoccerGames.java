8dbde1ccda193c3965a20129b0205694fe94ff18
Nishat Khan <nishatkhan20431@hotmail.ca>
2021-07-14 17:22:53 -0400

    Made an inner class within SoccerGames.java to store article info to be displayed in the RecyclerView

--- app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/SoccerGames.java
+++ app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/SoccerGames.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.RecyclerView;
 
+import java.util.Date;
+
 public class SoccerGames extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,5 +14,24 @@ public class SoccerGames extends AppCompatActivity {
         setContentView(R.layout.recycler_layout); //layout to list the article titles
 
         RecyclerView articlesRecyclerView = findViewById(R.id.myrecycler);
+    }
+
+    //this class is similar to the ChatMessage class in the RecyclerView lab (week 5) - used to store and get the information for each article to be displayed in the RecyclerView
+    private class ArticleInfo {
+        String title;
+        String datePublished;
+
+        public ArticleInfo(String title, String datePublished) {
+            this.title = title;
+            this.datePublished = datePublished;
+        }
+
+        public String getTitle() {
+            return title;
+        }
+
+        public String getDatePublished() {
+            return datePublished;
+        }
     }
 }
