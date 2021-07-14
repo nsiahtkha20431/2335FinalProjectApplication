1bfe34d7c6f9627217a5a12ce466e0522a982a99
Nishat Khan <nishatkhan20431@hotmail.ca>
2021-07-14 16:48:49 -0400

    Added a SoccerGames class for the Soccer games article app

---
+++ app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/SoccerGames.java
@@ -0,0 +1,13 @@
+package algonquin.cst2335.a2335finalprojectapplication;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SoccerGames extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.recycler_layout); //layout to list the article titles
+    }
+}
