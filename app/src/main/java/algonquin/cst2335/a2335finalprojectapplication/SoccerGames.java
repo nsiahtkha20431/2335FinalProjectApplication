f27d7b3b05a495d240d9f244e8900d30dc7bb965
Nishat Khan <nishatkhan20431@hotmail.ca>
2021-07-14 17:56:12 -0400

    Fixed Adapter according to prof instructions and removed ViewHolder for now. Will do ViewHolder for next commit

--- app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/SoccerGames.java
+++ app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/SoccerGames.java
@@ -2,6 +2,7 @@ package algonquin.cst2335.a2335finalprojectapplication;
 
 import android.os.Bundle;
 import android.view.View;
+import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,14 +11,31 @@ import androidx.recyclerview.widget.RecyclerView;
 import java.util.Date;
 
 public class SoccerGames extends AppCompatActivity {
-    ArticleAdapter adapter = new ArticleAdapter;
+    ArticleAdapter adapter = new ArticleAdapter();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recycler_layout); //layout to list the article titles
-
         RecyclerView articlesRecyclerView = findViewById(R.id.myrecycler);
+        articlesRecyclerView.setAdapter(new ArticleAdapter());
+    }
+
+    private class ArticleAdapter extends RecyclerView.Adapter {
+        @Override
+        public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+            return null;
+        }
+
+        @Override
+        public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+
+        }
+
+        @Override
+        public int getItemCount() {
+            return 0;
+        }
     }
 
     //this class is similar to the ChatMessage class in the RecyclerView lab (week 5) - used to store and get the information for each article to be displayed in the RecyclerView
@@ -37,16 +55,5 @@ public class SoccerGames extends AppCompatActivity {
         public String getDatePublished() {
             return datePublished;
         }
-    }
-
-    private class ArticleViewHolder extends RecyclerView.ViewHolder {
-
-        public ArticleViewHolder(View itemView) {
-            super(itemView);
-        }
-    }
-
-    private class ArticleAdapter extends ArticleViewHolder.Adapter<ArticleViewHolder> {
-
     }
 }
