c16d9304cba78000a61c7ae1b3f5c87da89c93c0
Nishat Khan <nishatkhan20431@hotmail.ca>
2021-07-14 17:07:40 -0400

    Added RecyclerView to java class

--- app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/SoccerGames.java
+++ app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/SoccerGames.java
@@ -3,11 +3,14 @@ package algonquin.cst2335.a2335finalprojectapplication;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class SoccerGames extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.recycler_layout); //layout to list the article titles
+
+        RecyclerView articlesRecyclerView = findViewById(R.id.myrecycler);
     }
 }
