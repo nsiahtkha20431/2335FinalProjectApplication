4a9b7a2b9773d60dad905ac6fb1cc5474c9fea8f
Nishat Khan <nishatkhan20431@hotmail.ca>
2021-07-14 10:12:00 -0400

    Initial commit

---
+++ app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/MainActivity.java
@@ -0,0 +1,14 @@
+package algonquin.cst2335.a2335finalprojectapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
