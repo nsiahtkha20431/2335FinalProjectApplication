20cae728515ef53848ec2dda8dc152edd7afa488
Emma <mcar0058@algonquinlive.com>
2021-07-17 13:40:40 -0400

    Fragments partially implemented

---
+++ app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/StopDetailsFragment.java
@@ -0,0 +1,30 @@
+package algonquin.cst2335.a2335finalprojectapplication;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.fragment.app.Fragment;
+
+public class StopDetailsFragment extends Fragment {
+    private int stopNo;
+
+    public StopDetailsFragment(int stopNo) {
+        this.stopNo = stopNo;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState){
+        View detailsView;
+        if (stopNo == 0000) {
+            detailsView = inflater.inflate(R.layout.add_stop_layout, container, false);
+        } else {
+            //get api result for stop no
+            //inflate stop detail layout
+            detailsView = inflater.inflate(R.layout.stop_details_layout, container, false);
+            //populate detail fields
+        }
+        return detailsView;
+    }
+}
