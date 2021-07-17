f03e722ac886a7329c59f05160b2c10f49a67836
Emma <mcar0058@algonquinlive.com>
2021-07-16 20:31:04 -0400

    Recycler View Implemented

--- app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/MainActivity.java
@@ -2,7 +2,9 @@ package algonquin.cst2335.a2335finalprojectapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button ocTranspo = findViewById(R.id.ocTranspoButton);
+        ocTranspo.setOnClickListener(clk -> {
+            Intent nextPage = new Intent(MainActivity.this, OCTranspoActivity.class);
+            startActivity(nextPage);
+        });
+        Button electricCar  = findViewById(R.id.electricCarButton);
+        Button movieInfo = findViewById(R.id.movieInfoButton);
+        Button soccerGames = findViewById(R.id.soccerGamesButton);
+
+
     }
 }
