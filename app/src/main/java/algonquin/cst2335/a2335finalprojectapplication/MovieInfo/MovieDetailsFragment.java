f6d7945fb607ca675bfa74ea8c38fbe3227a1cf3
041005249 <lebl0438@algonquinlive.com>
2021-07-18 00:17:47 -0400

    Connected SearchFragment with DetailsFragment.

--- app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/MovieInfo/MovieDetailsFragment.java
+++ app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/MovieInfo/MovieDetailsFragment.java
@@ -2,10 +2,12 @@ package algonquin.cst2335.a2335finalprojectapplication.MovieInfo;
 
 import android.content.Context;
 import android.content.SharedPreferences;
+import android.media.Image;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -16,14 +18,37 @@ import algonquin.cst2335.a2335finalprojectapplication.R;
 public class MovieDetailsFragment extends Fragment {
 
     TextView title;
+    TextView year;
+    TextView rating;
+    TextView runtime;
+    TextView actors;
+    TextView plot;
+    ImageView poster;
+
+    MovieSearchFragment.MovieInfo movieInfo;
     @Override
     public View onCreateView( LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View detailsLayout =inflater.inflate(R.layout.movie_details_layout, container, false);
         SharedPreferences prefs = this.getActivity().getSharedPreferences("Data", Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = prefs.edit();
         title = detailsLayout.findViewById(R.id.title_message);
-        title.setText(prefs.getString("TitleName", ""));
+        title.setText(movieInfo.getTitle());
+        year = detailsLayout.findViewById(R.id.year_message);
+        year.setText(movieInfo.getYear());
+        rating = detailsLayout.findViewById(R.id.rating_message);
+        rating.setText(movieInfo.getRating());
+        runtime = detailsLayout.findViewById(R.id.runtime_message);
+        runtime.setText(movieInfo.getRuntime());
+        actors = detailsLayout.findViewById(R.id.actors_message);
+        actors.setText(movieInfo.getActors());
+        plot = detailsLayout.findViewById(R.id.plot_message);
+        plot.setText(movieInfo.getPlot());
+        //TODO ImageView with ASyncTask
 
         return detailsLayout;
+    }
+
+    public MovieDetailsFragment(MovieSearchFragment.MovieInfo movieInfo){
+            this.movieInfo = movieInfo;
     }
 }
--- app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/MovieInfo/MovieInfoActivity.java
+++ app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/MovieInfo/MovieInfoActivity.java
@@ -1,29 +1,12 @@
 package algonquin.cst2335.a2335finalprojectapplication.MovieInfo;
 
-import android.content.Context;
-import android.content.SharedPreferences;
+
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
+
 import android.widget.EditText;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
-import com.android.volley.Request;
-import com.android.volley.RequestQueue;
-import com.android.volley.Response;
-import com.android.volley.VolleyError;
-import com.android.volley.toolbox.JsonObjectRequest;
-import com.android.volley.toolbox.Volley;
-import com.google.android.material.snackbar.Snackbar;
-
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.io.UnsupportedEncodingException;
-import java.net.URLEncoder;
 
 import algonquin.cst2335.a2335finalprojectapplication.FinalOpenHelper;
 import algonquin.cst2335.a2335finalprojectapplication.R;
@@ -42,7 +25,6 @@ public class MovieInfoActivity extends AppCompatActivity {
     String actors;
     String plot;
     String URL;
-    MovieDetailsFragment detailsFrag = new MovieDetailsFragment();
     MovieSearchFragment searchFrag = new MovieSearchFragment();
 
     @Override
@@ -54,5 +36,12 @@ public class MovieInfoActivity extends AppCompatActivity {
 
 
 
+    }
+
+    public void usrSearchedMovie(MovieSearchFragment.MovieInfo movieInfo) {
+
+        MovieDetailsFragment detailsFrag = new MovieDetailsFragment(movieInfo);
+
+        getSupportFragmentManager().beginTransaction().replace(R.id.movie_room, detailsFrag).commit();
     }
 }
