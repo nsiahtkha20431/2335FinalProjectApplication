f597dd36d60f7dc743480c39afb9b1c4e9af6100
Emma <mcar0058@algonquinlive.com>
2021-07-27 21:00:59 -0400

    Implemented icon toolbar

--- app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/MainActivity.java
@@ -1,9 +1,14 @@
 package algonquin.cst2335.a2335finalprojectapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 import algonquin.cst2335.a2335finalprojectapplication.ChargingStations.ChargingMainActivity;
 
@@ -20,29 +25,67 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         opener = new FinalOpenHelper(this);
         db = opener.getWritableDatabase();
+
+        Toolbar toolbar = findViewById(R.id.toolbar2);
+        setSupportActionBar(toolbar);
+
         Button ocTranspo = findViewById(R.id.ocTranspoButton);
         ocTranspo.setOnClickListener(clk -> {
             Intent nextPage = new Intent(MainActivity.this, OCTranspoActivity.class);
             startActivity(nextPage);
         });
-        Button electricCar  = findViewById(R.id.electricCarButton);
+
+        Button electricCar = findViewById(R.id.electricCarButton);
         electricCar.setOnClickListener(click -> {
             Intent nextPage = new Intent(MainActivity.this, ChargingMainActivity.class);
             startActivity(nextPage);
         });
 
         Button movieInfo = findViewById(R.id.movieInfoButton);
-        Button soccerGames = findViewById(R.id.soccerGamesButton);
-
-        soccerGames.setOnClickListener(clk -> {
-            Intent nextPage = new Intent( MainActivity.this, SoccerGames.class);
-            startActivity(nextPage);
-        });
         movieInfo.setOnClickListener(clk -> {
-            Intent nextPage = new Intent( MainActivity.this, MovieInfoActivity.class);
+            Intent nextPage = new Intent(MainActivity.this, MovieInfoActivity.class);
             startActivity(nextPage);
         });
+
+        Button soccerGames = findViewById(R.id.soccerGamesButton);
+        soccerGames.setOnClickListener(clk -> {
+            Intent nextPage = new Intent(MainActivity.this, SoccerGames.class);
+            startActivity(nextPage);
+        });
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.nav_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Intent nextPage;
+        switch(item.getItemId()) {
+            case R.id.ic_ocTranspo:
+                nextPage = new Intent(MainActivity.this, OCTranspoActivity.class);
+                startActivity(nextPage);
+                break;
+            case R.id.ic_electric:
+                nextPage = new Intent(MainActivity.this, ChargingMainActivity.class);
+                startActivity(nextPage);
+                break;
+            case R.id.ic_movie:
+                nextPage = new Intent( MainActivity.this, MovieInfoActivity.class);
+                startActivity(nextPage);
+                break;
+            case R.id.ic_soccer:
+                nextPage = new Intent( MainActivity.this, SoccerGames.class);
+                startActivity(nextPage);
+                break;
+        }
+
+        return true;
     }
 }
