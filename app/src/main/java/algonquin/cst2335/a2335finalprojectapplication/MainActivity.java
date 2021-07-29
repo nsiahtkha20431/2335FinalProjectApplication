f8666b0dfece15920d8e40980c749ca2b9ebd3e6
Emma <mcar0058@algonquinlive.com>
2021-07-29 13:15:51 -0400

    Implemented navigation drawer

--- app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/MainActivity.java
@@ -1,17 +1,24 @@
 package algonquin.cst2335.a2335finalprojectapplication;
 
+
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.GravityCompat;
+import androidx.drawerlayout.widget.DrawerLayout;
 
+import android.app.Notification;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
-import android.widget.Button;
-import algonquin.cst2335.a2335finalprojectapplication.ChargingStations.ChargingMainActivity;
 
+import com.google.android.material.navigation.NavigationView;
+
+import algonquin.cst2335.a2335finalprojectapplication.ChargingStations.ChargingMainActivity;
 import algonquin.cst2335.a2335finalprojectapplication.OCTranspo.OCTranspoActivity;
 import algonquin.cst2335.a2335finalprojectapplication.SoccerGames.SoccerGames;
 import algonquin.cst2335.a2335finalprojectapplication.MovieInfo.MovieInfoActivity;
@@ -26,41 +33,30 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        this.setTitle("");
+
         opener = new FinalOpenHelper(this);
         db = opener.getWritableDatabase();
 
         Toolbar toolbar = findViewById(R.id.toolbar2);
         setSupportActionBar(toolbar);
 
-        Button ocTranspo = findViewById(R.id.ocTranspoButton);
-        ocTranspo.setOnClickListener(clk -> {
-            Intent nextPage = new Intent(MainActivity.this, OCTranspoActivity.class);
-            startActivity(nextPage);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.open, R.string.close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+        NavigationView navigation = findViewById(R.id.nav_view);
+        navigation.setNavigationItemSelectedListener( (item) -> {
+            onOptionsItemSelected(item);
+            drawer.closeDrawer(GravityCompat.START);
+            return false;
         });
-
-        Button electricCar = findViewById(R.id.electricCarButton);
-        electricCar.setOnClickListener(click -> {
-            Intent nextPage = new Intent(MainActivity.this, ChargingMainActivity.class);
-            startActivity(nextPage);
-        });
-
-        Button movieInfo = findViewById(R.id.movieInfoButton);
-        movieInfo.setOnClickListener(clk -> {
-            Intent nextPage = new Intent(MainActivity.this, MovieInfoActivity.class);
-            startActivity(nextPage);
-        });
-
-        Button soccerGames = findViewById(R.id.soccerGamesButton);
-        soccerGames.setOnClickListener(clk -> {
-            Intent nextPage = new Intent(MainActivity.this, SoccerGames.class);
-            startActivity(nextPage);
-        });
-
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.nav_menu, menu);
+        MenuInflater infltr = getMenuInflater();
+        infltr.inflate(R.menu.nav_menu, menu);
         return true;
     }
 
