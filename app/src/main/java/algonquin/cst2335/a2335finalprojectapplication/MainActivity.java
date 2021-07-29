70d05914bddc60a3089609ea8459f66b8e0ab785
Emma <mcar0058@algonquinlive.com>
2021-07-29 13:37:41 -0400

    final

--- app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/MainActivity.java
+++ app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/MainActivity.java
@@ -1,23 +1,17 @@
 package algonquin.cst2335.a2335finalprojectapplication;
 
-
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.core.view.GravityCompat;
 import androidx.drawerlayout.widget.DrawerLayout;
-
-import android.app.Notification;
 import android.content.Intent;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
-
 import com.google.android.material.navigation.NavigationView;
-
 import algonquin.cst2335.a2335finalprojectapplication.ChargingStations.ChargingMainActivity;
 import algonquin.cst2335.a2335finalprojectapplication.OCTranspo.OCTranspoActivity;
 import algonquin.cst2335.a2335finalprojectapplication.SoccerGames.SoccerGames;
