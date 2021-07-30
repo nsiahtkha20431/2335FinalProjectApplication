b392ed7d3128439a56b4c558cf7289f2f03351ba
041005249 <lebl0438@algonquinlive.com>
2021-07-30 02:35:45 -0400

    Small tweak to onMenuItemClick (added switch case).

--- app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/MovieInfo/MovieInfoActivity.java
+++ app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/MovieInfo/MovieInfoActivity.java
@@ -65,62 +65,71 @@ public class MovieInfoActivity extends AppCompatActivity {
             @Override
             public boolean onMenuItemClick(MenuItem item) {
 
-                if (item.getItemId() == R.id.search) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.movie_room, searchFrag).commit();
-                    item.setChecked(true);
-                    if (Locale.getDefault().getDisplayLanguage().equals("français")){
-                        toolbar.setTitle("Recherche des Films");
-                    }
-                    else{
-                        toolbar.setTitle("Search Movie");
-                    }
+                switch(item.getItemId()) {
 
-                } else if (item.getItemId() == R.id.saved) {
-                    getSupportFragmentManager().beginTransaction().replace(R.id.movie_room, savedFrag).commit();
-                    item.setChecked(true);
-                    if (Locale.getDefault().getDisplayLanguage().equals("français")){
-                        toolbar.setTitle("Films Sauvegardées");
-                    }
-                    else{
-                        toolbar.setTitle("Saved Movies");
-                    }
+                    case R.id.search:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.movie_room, searchFrag).commit();
+                        item.setChecked(true);
+                        if (Locale.getDefault().getDisplayLanguage().equals("français")){
+                            toolbar.setTitle("Recherche des Films");
+                        }
+                        else{
+                            toolbar.setTitle("Search Movie");
+                        }
 
-                } else if (item.getItemId() == R.id.help){
 
-                    if (Locale.getDefault().getDisplayLanguage().equals("français")) {
-                        new AlertDialog.Builder(MovieInfoActivity.this)
-                                .setTitle("Aide")
-                                .setMessage("   Cette application, Movie Information, permet à l'utilisateur d'effectuer une recherche dans la base de " +
-                                        "données utilisée sur omdbapi.com en tapant simplement le texte contenu dans un titre de film." +
-                                        " \n   Si le titre du film saisi est introuvable ou n'existe tout simplement pas, un message s'affiche pour informer qu'aucun film n'a été trouvé." +
-                                        "\n   Une fois le film trouvé, il fournit des détails sur le film, qui peuvent être enregistrés localement sur l'appareil avec les informations et " +
-                                        "l'image du film. \n   Les films sauvegardés peuvent être trouvés en utilisant les options de menu à partir desquelles vous avez " +
-                                        "trouvé ce bouton d'aide. La liste sauvegardée s'affiche à la place de la page actuelle avec le nom du film, l'année et le classement," +
-                                        " ainsi que l'image. \n   En cliquant sur un film, les détails enregistrés s'affichent. \n   Une fois encore, vous pouvez fermer les détails" +
-                                        " ou continuer à consulter votre liste sauvegardée et à effectuer des recherches comme bon vous semble!")
-                                .setNegativeButton("Fermer", (diallog, cl) -> {
-                                    //does nothing, closes.
-                                })
-                                .create().show();
-                    }
-                    else{
-                        new AlertDialog.Builder(MovieInfoActivity.this)
-                                .setTitle("Help")
-                                .setMessage("This app, Movie Information, allows the user to search throughout the database utilized on omdbapi.com by simply typing" +
-                                        " text that is contained within a movie title. \n   If the movie title entered can't be found or simply does not exist, a message will" +
-                                        " be shown to inform that no movie was found. \n   Once the movie has been found, it will provide details regarding the movie from" +
-                                        " which can be saved onto the device locally along with the information and movie image. \n   Saved movies can be found using the" +
-                                        " menu options from which you found this help button. The saved list will be displayed instead of the current page along with" +
-                                        " the movie names, year and rating with the image. \n   Upon clicking a movie, the saved details will be displayed." +
-                                        "\n   Once again, you may close the details or continue to look at your saved list and search as you like! ")
-                                .setNegativeButton("Close", (dialog, cl) -> {
-                                    //does nothing, closes.
-                                })
-                                .create().show();
-                    }
+                        break;
+                    case R.id.saved:
+                        getSupportFragmentManager().beginTransaction().replace(R.id.movie_room, savedFrag).commit();
+                        item.setChecked(true);
+                        if (Locale.getDefault().getDisplayLanguage().equals("français")){
+                            toolbar.setTitle("Films Sauvegardées");
+                        }
+                        else{
+                            toolbar.setTitle("Saved Movies");
+                        }
+
+
+                        break;
+                    case R.id.help:
+                        if (Locale.getDefault().getDisplayLanguage().equals("français")) {
+                            new AlertDialog.Builder(MovieInfoActivity.this)
+                                    .setTitle("Aide")
+                                    .setMessage("   Cette application, Movie Information, permet à l'utilisateur d'effectuer une recherche dans la base de " +
+                                            "données utilisée sur omdbapi.com en tapant simplement le texte contenu dans un titre de film." +
+                                            " \n   Si le titre du film saisi est introuvable ou n'existe tout simplement pas, un message s'affiche pour informer qu'aucun film n'a été trouvé." +
+                                            "\n   Une fois le film trouvé, il fournit des détails sur le film, qui peuvent être enregistrés localement sur l'appareil avec les informations et " +
+                                            "l'image du film. \n   Les films sauvegardés peuvent être trouvés en utilisant les options de menu à partir desquelles vous avez " +
+                                            "trouvé ce bouton d'aide. La liste sauvegardée s'affiche à la place de la page actuelle avec le nom du film, l'année et le classement," +
+                                            " ainsi que l'image. \n   En cliquant sur un film, les détails enregistrés s'affichent. \n   Une fois encore, vous pouvez fermer les détails" +
+                                            " ou continuer à consulter votre liste sauvegardée et à effectuer des recherches comme bon vous semble!")
+                                    .setNegativeButton("Fermer", (diallog, cl) -> {
+                                        //does nothing, closes.
+                                    })
+                                    .create().show();
+                        }
+                        else{
+                            new AlertDialog.Builder(MovieInfoActivity.this)
+                                    .setTitle("Help")
+                                    .setMessage("This app, Movie Information, allows the user to search throughout the database utilized on omdbapi.com by simply typing" +
+                                            " text that is contained within a movie title. \n   If the movie title entered can't be found or simply does not exist, a message will" +
+                                            " be shown to inform that no movie was found. \n   Once the movie has been found, it will provide details regarding the movie from" +
+                                            " which can be saved onto the device locally along with the information and movie image. \n   Saved movies can be found using the" +
+                                            " menu options from which you found this help button. The saved list will be displayed instead of the current page along with" +
+                                            " the movie names, year and rating with the image. \n   Upon clicking a movie, the saved details will be displayed." +
+                                            "\n   Once again, you may close the details or continue to look at your saved list and search as you like! ")
+                                    .setNegativeButton("Close", (dialog, cl) -> {
+                                        //does nothing, closes.
+                                    })
+                                    .create().show();
+                        }
+
+
+                        break;
 
                 }
-                return false;
+
+                return true;
             }
 
         });
