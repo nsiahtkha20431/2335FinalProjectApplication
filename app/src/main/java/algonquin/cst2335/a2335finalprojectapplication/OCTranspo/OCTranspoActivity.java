06d2753c4eb4d60bd7bce77951196941a0f4778c
Emma <mcar0058@algonquinlive.com>
2021-07-31 12:26:57 -0400

    Added toolbar with functioning menu items

--- app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/OCTranspo/OCTranspoActivity.java
+++ app/src/main/java/algonquin/cst2335/a2335finalprojectapplication/OCTranspo/OCTranspoActivity.java
@@ -1,11 +1,17 @@
 package algonquin.cst2335.a2335finalprojectapplication.OCTranspo;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
+import android.app.AlertDialog;
 import android.content.ContentValues;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 
 import com.google.android.material.snackbar.Snackbar;
@@ -22,9 +28,50 @@ public class OCTranspoActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_octranspo);
+        setTitle("");
         prefs = getSharedPreferences("OCT_Data", Context.MODE_PRIVATE);
         StopListFragment listFrag = new StopListFragment();
         getSupportFragmentManager().beginTransaction().add(R.id.listFrag, listFrag, "list").commit();
+        Toolbar toolbar = findViewById(R.id.oct_toolbar);
+        setSupportActionBar(toolbar);
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.oct_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_add_stop:
+                StopDetailsFragment newFrag = new StopDetailsFragment();
+                getSupportFragmentManager().beginTransaction().replace(R.id.listFrag, newFrag, "detail").addToBackStack(null).commit();
+                break;
+            case R.id.menu_help:
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setTitle("Help")
+                        .setMessage("OC Transpo Stop Selector displays live stop and route data from" +
+                                "the OCTranspo server.\n\nSelect a stop number to display incoming bus routes." +
+                                "\n\nSelect a route number to display route details and upcoming trips." +
+                                "\n\nTo add a stop to your list select the plus sign from the toolbar or \"Add Stop\" " +
+                                "from the navigation menu.")
+                        .setPositiveButton("OK", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        });
+                AlertDialog alert = builder.create();
+                alert.show();
+                break;
+            case R.id.menu_exit:
+                finish();
+                break;
+        }
+        return true;
     }
 
     public void addStop(String stop, String description) {
